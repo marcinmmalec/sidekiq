833a89765fbfe3f73f0e2ab9bc2eb1f40c64cb84
Marcin Mariusz Malec <martinmmalec@gmail.com>
2021-07-19 16:28:36 -0600

    Try even more

--- lib/sidekiq/web/helpers.rb
+++ lib/sidekiq/web/helpers.rb
@@ -178,7 +178,7 @@ module Sidekiq
     end
 
     def root_path
-      "#{Sidekiq::Web.redirect_path}#{env["SCRIPT_NAME"]}"
+      "#{Sidekiq::Web.redirect_path}#{env["SCRIPT_NAME"]}/"
     end
 
     def current_path
